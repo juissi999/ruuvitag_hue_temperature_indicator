1bfba7c2a2655b8fcb77c67a1769fd5b4a50d3e9
juissi999 <jussi.kaatiala@gmail.com>
2023-05-21 23:48:17 +0300

    Add tinydb message-broker dependency

--- ruuvi.py
+++ ruuvi.py
@@ -1,8 +1,17 @@
+import os
+
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
+from tinydb import TinyDB
+from dotenv import load_dotenv
 
 def handle_data(found_data):
   print('MAC ' + found_data[0])
   print(found_data[1])
 
 if __name__ == '__main__':
+  # load environment variables
+  load_dotenv()
+
+  db = TinyDB(os.getenv('DBFILE'))
+
   RuuviTagSensor.get_datas(handle_data)
