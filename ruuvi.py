0f872458c53fbf3825ce8947b880abed26adbcbb
juissi999 <jussi.kaatiala@gmail.com>
2023-05-22 00:09:25 +0300

    Add ruuvi sensor upsert data to db

--- ruuvi.py
+++ ruuvi.py
@@ -1,17 +1,19 @@
 import os
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+db = TinyDB(os.getenv('DBFILE'))
+Sensor = Query()
 
 def handle_data(found_data):
   print('MAC ' + found_data[0])
   print(found_data[1])
+  db.upsert({'MAC': found_data[0], 'data': found_data[1]}, Sensor.MAC == found_data[0])
 
 if __name__ == '__main__':
-  # load environment variables
-  load_dotenv()
-
-  db = TinyDB(os.getenv('DBFILE'))
-
   RuuviTagSensor.get_datas(handle_data)
